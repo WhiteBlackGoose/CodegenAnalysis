5d9bc6db6574f43199f52aec94ac26220b89cc16
WhiteBlackGoose <wbg@angouri.org>
2022-08-08 12:00:57 +0000

    wip

--- Samples/CodegenAnalysis.Assertions.Sample/UnitTest1.cs
+++ Samples/CodegenAnalysis.Assertions.Sample/UnitTest1.cs
@@ -2,11 +2,29 @@ using Xunit;
 
 namespace CodegenAnalysis.Assertions.Sample;
 
+public class LibraryClass
+{
+    public static readonly float Pi = 3.14f;
+
+    public static bool IsPiGreaterThan3()
+    {
+        return Pi > 3f;
+    }
+}
+
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        CodegenInfo.Obtain(() => LibraryClass.IsPiGreaterThan3(), CompilationTier.Default)
+            .ShouldContainBranches(1);
+    }
 
+    [Fact]
+    public void Test2()
+    {
+        CodegenInfo.Obtain(() => LibraryClass.IsPiGreaterThan3(), CompilationTier.Tier1)
+            .ShouldContainBranches(0);
     }
 }
