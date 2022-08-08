1cbe3695d164cd9987c46801347f087b533c49be
WhiteBlackGoose <wbg@angouri.org>
2022-08-08 12:46:13 +0000

    Benchmark sample

---
+++ Samples/CodegenAnalysis.Benchmarks.Sample/Program.cs
@@ -0,0 +1,47 @@
+﻿using CodegenAnalysis.Benchmarks;
+using CodegenAnalysis;
+using System.Runtime.CompilerServices;
+
+CodegenBenchmarkRunner.Run<A>();
+
+[CAJob(Tier = CompilationTier.Default),
+ CAJob(Tier = CompilationTier.Tier1)]
+
+[CAColumn(CAColumn.Branches),
+ CAColumn(CAColumn.Calls), 
+ CAColumn(CAColumn.CodegenSize), 
+ CAColumn(CAColumn.StaticStackAllocations)]
+
+[CAExport(Export.Html),
+ CAExport(Export.Md)]
+public class A
+{
+    [CAAnalyze(3.5f)]
+    [CAAnalyze(13.5f)]
+    public static float Heavy(float a)
+    {
+        var b = Do1(a);
+        var c = Do1(b);
+        if (a > 10)
+            c += Aaa(a);
+        return c + b;
+    }
+
+    [CAAnalyze(6f)]
+    public static float Square(float a)
+    {
+        return a * a;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static float Do1(float a)
+    {
+        return a * 2;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static float Aaa(float h)
+    {
+        return h * h * h;
+    }
+}
