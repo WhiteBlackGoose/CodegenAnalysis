40b920ba4b1801101492b969a86d78827f0508cb
WhiteBlackGoose <wbg@angouri.org>
2022-08-08 12:12:26 +0000

    assertions sample done

--- Samples/CodegenAnalysis.Assertions.Sample/UnitTest1.cs
+++ Samples/CodegenAnalysis.Assertions.Sample/UnitTest1.cs
@@ -10,6 +10,18 @@ public class LibraryClass
     {
         return Pi > 3f;
     }
+
+    public static decimal RandomComputations()
+    {
+        decimal a1 = 1; decimal a2 = 2; decimal a3 = 3;
+        a1 = a2 + a3; a2 = a1 + a3; a3 = a1 + a2 + a3; a2 = a1 + a3;
+        return a1 / a2;
+    }
+
+    public static float Cbr(float g)
+    {
+        return g * g * g;
+    }
 }
 
 public class UnitTest1
@@ -18,13 +30,29 @@ public class UnitTest1
     public void Test1()
     {
         CodegenInfo.Obtain(() => LibraryClass.IsPiGreaterThan3(), CompilationTier.Default)
-            .ShouldContainBranches(1);
+            .ShouldHaveCalls(1);
     }
 
     [Fact]
     public void Test2()
     {
         CodegenInfo.Obtain(() => LibraryClass.IsPiGreaterThan3(), CompilationTier.Tier1)
-            .ShouldContainBranches(0);
+            .ShouldHaveCalls(0)
+            .ShouldHaveBranches(0);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        CodegenInfo.Obtain(() => LibraryClass.RandomComputations(), CompilationTier.Tier1)
+            .ShouldStaticStackAllocateNoMoreThan(32); // should fail
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        CodegenInfo.Obtain(() => LibraryClass.Cbr(3f), CompilationTier.Tier1)
+            .ShouldBeOfSize(size => size < 16)
+            .ShouldStaticStackAllocateNoMoreThan(0);
     }
 }
